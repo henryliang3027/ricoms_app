3b79481df49ac315ebcb933f805b5a6c6011c9e6
Henry.Liang <henryliang3027@gmail.com>
2023-02-10 15:14:16 +0800

    refactor: :recycle: remove unnecessary import

--- lib/advanced/view/batch_setting_view/device_setting_result_form.dart
+++ lib/advanced/view/batch_setting_view/device_setting_result_form.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ricoms_app/advanced/bloc/batch_setting/device_setting_result/device_setting_result_bloc.dart';
