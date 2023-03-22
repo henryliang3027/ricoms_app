47e644cda8170da883bc685c9c46e911d84fa543
Henry.Liang <henryliang3027@gmail.com>
2023-03-22 13:41:54 +0800

    refactor: :recycle: update master slave info class logit

--- lib/utils/master_slave_info.dart
+++ lib/utils/master_slave_info.dart
@@ -6,6 +6,7 @@ class MasterSlaveServerInfo {
   static String slaveServerIP = '';
   static String onlineServerIP = '';
 
+  /// 取得目前 online server ip
   static Future<String> getOnlineServerIP({
     required String loginIP,
     required Dio dio,
@@ -26,6 +27,7 @@ class MasterSlaveServerInfo {
         int model = data['data'][0]['model'];
 
         if (model == 1) {
+          // 主從模式
           MasterSlaveServerInfo.masterServerIP =
               data['data'][0]['server_master'];
           MasterSlaveServerInfo.slaveServerIP = data['data'][0]['server_slave'];
@@ -33,13 +35,17 @@ class MasterSlaveServerInfo {
               data['data'][0]['server_online'];
           return MasterSlaveServerInfo.onlineServerIP;
         } else {
+          // 非主從模式
+          // 全部設定為 login ip
           MasterSlaveServerInfo.masterServerIP = loginIP;
           MasterSlaveServerInfo.slaveServerIP = loginIP;
           MasterSlaveServerInfo.onlineServerIP = loginIP;
           return loginIP;
         }
       } else {
-        return loginIP;
+        // 如果api取得資料失敗, 判斷 online server ip 是否不為空
+        // 不為空則回傳 onlineServerIP
+        return onlineServerIP != '' ? onlineServerIP : loginIP;
       }
     } on DioError catch (_) {
       return MasterSlaveServerInfo.slaveServerIP;
